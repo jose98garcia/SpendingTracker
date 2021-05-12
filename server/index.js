4a6da4d6558ac30b55ca89cd84889cb5373e0674
jose98garcia <garcia.jose804@gmail.com>
2021-05-11 22:44:27 -0500

    simple server created

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
---
+++ server/routes/api/posts.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//GET 
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//ADD
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        category: req.body.category,
+        transaction: req.body.transaction,
+        transactionCost: req.body.transactionCost,        
+        createdAt: new Date()
+    });
+
+    res.status(201).send();
+});
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send();
+})
+
+async function loadPostsCollection(){
+    const client = 
+        await mongodb.MongoClient.connect('mongodb+srv://tester:tester1234@stracker.ckqna.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        {useNewUrlParser: true, useUnifiedTopology: true});
+        
+    return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
